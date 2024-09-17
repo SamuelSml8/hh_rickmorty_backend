b936f8472da210c34c273a1ce5280eb6750bc859
Samuel Sml <veramirandasamuel6@gmail.com>
2024-09-17 15:35:36 -0500

    feat: Add endpoint to list and filter characters by name with pagination

--- src/modules/characters/controllers/characters.controller.ts
+++ src/modules/characters/controllers/characters.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, ParseIntPipe, Post, Query } from '@nestjs/common';
 import { CharactersService } from '../services/characters.service';
 import { ResponseApiHh } from 'src/utils/interfaces/response.interface';
 import { Character } from 'src/entities/character.entity';
@@ -10,5 +10,16 @@ export class CharactersController {
   @Post('import')
   async importCharacters(): Promise<ResponseApiHh<Character[]>> {
     return this.charactersService.importCharacters();
+  }
+
+  @Get('')
+  async getCharactersByName(
+    @Query('name') name: string,
+    @Query('page')
+    page: number = 1,
+    @Query('limit')
+    limit: number = 20,
+  ): Promise<ResponseApiHh<Character[]>> {
+    return this.charactersService.getCharactersByName(name, page, limit);
   }
 }
