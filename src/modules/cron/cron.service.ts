ad507455751a3c07471f382f6af8a89867005f4e
Samuel Sml <veramirandasamuel6@gmail.com>
2024-09-17 20:43:34 -0500

    refactor: replace console.log with NestJS logger in CronService for better logging

--- src/modules/cron/cron.service.ts
+++ src/modules/cron/cron.service.ts
@@ -1,14 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { CharactersService } from '../characters/services/characters.service';
 
 @Injectable()
 export class CronService {
+  private readonly logger = new Logger(CronService.name);
+
   constructor(private readonly charactersService: CharactersService) {}
 
-  @Cron(CronExpression.EVERY_30_MINUTES)
+  @Cron(CronExpression.EVERY_10_MINUTES)
   async handleCron() {
-    console.log('Running cron job to refresh characters every 30 minutes');
-    await this.charactersService.refreshCharacters();
+    this.logger.log('Running cron job to refresh characters every 30 minutes');
+    try {
+      await this.charactersService.refreshCharacters();
+    } catch (error) {
+      this.logger.error(
+        'Error occurred while refreshing characters',
+        error.stack,
+      );
+    }
   }
 }
